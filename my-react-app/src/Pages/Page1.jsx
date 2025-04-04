701bb06fbf2e2308a1586ffb26317b7ece4a1b1a
GregElDeiry <gce4447@gmail.com>
2025-04-04 18:16:51 -0400

    made border smaller to fill out frame

--- my-react-app/src/Pages/Page1.jsx
+++ my-react-app/src/Pages/Page1.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export default function Page1() {
     return (
         <>
-            <h1>This is a test</h1>
+            <h1 >This is a test</h1>
             {/* <Link to="/">Home </Link>
             <Link to="/Page1">Page1 </Link> */}
         </>
