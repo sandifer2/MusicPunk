964b1c2c5986a560e2466bf7ab5669c0f6c6670c
GregElDeiry <gce4447@gmail.com>
2025-04-06 18:48:43 -0400

    Just made a page.css file to use as styling on pages

--- my-react-app/src/Pages/Page1.jsx
+++ my-react-app/src/Pages/Page1.jsx
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
-
+import '../CSS/Page.css'
 
 
 export default function Page1() {
     return (
-        <>
+        <div className="page-container">
             <h1 >This is a test</h1>
             {/* <Link to="/">Home </Link>
             <Link to="/Page1">Page1 </Link> */}
-        </>
+        </div>
     );
 }
