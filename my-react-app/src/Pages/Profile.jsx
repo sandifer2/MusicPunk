51b46abce42ec8c53bf7c059725d83936220e6af
GregElDeiry <gce4447@gmail.com>
2025-04-06 20:30:04 -0400

    making a profile page css file

--- my-react-app/src/Pages/Profile.jsx
+++ my-react-app/src/Pages/Profile.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
+import '../CSS/ProfilePage.css'
 
 export default function Profile(){
     return(
-        <>
+        <div className = "profile-container">
             <h1>This is the profile page</h1>     
         
         
-        </>
+        </div>
     );
 }
