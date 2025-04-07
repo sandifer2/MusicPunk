b4688ff70d8cf844cc95b6802a380cd799a64f09
GregElDeiry <gce4447@gmail.com>
2025-04-06 21:05:42 -0400

    minor tweak of css

--- my-react-app/src/Pages/Profile.jsx
+++ my-react-app/src/Pages/Profile.jsx
@@ -4,9 +4,7 @@ import '../CSS/ProfilePage.css'
 export default function Profile(){
     return(
         <div className = "profile-container">
-            <h1>This is the profile page</h1>     
-        
-        
+            <h1>This is the profile page</h1>    
         </div>
     );
 }
