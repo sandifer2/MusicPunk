d177a966eb8b5998ba580f98074678ec15159d96
GregElDeiry <gce4447@gmail.com>
2025-04-06 20:05:36 -0400

    I made page 2 have proper CSS

--- my-react-app/src/Pages/Page2.jsx
+++ my-react-app/src/Pages/Page2.jsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
+import '../CSS/Page.css'
 
 export default function Page2() {
     return (
-        <>
+        <div className="page-container">
             <h1>This is a test for Page 2</h1>
             {/* <nav>
                 <Link to="/">Home</Link>
                 <br />
                 <Link to="/Page1">Page 1</Link>
             </nav> */}
-        </>
+        </div>
     );
 }
